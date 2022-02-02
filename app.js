9231e83b295764a2ed91f40563a75f562f21e1b9
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-02-02 11:08:22 +0100

    make product pages dynamic with Handlebars

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+
 app.use(express.static('public'));
+
 
 app.get("/", function(request, response, next){
     response.sendFile(__dirname + '/views/home.html');
@@ -11,19 +16,33 @@ app.get("/contact", function(request, response, next){
     response.sendFile(__dirname + '/views/contact.html');
 });
 
+
 app.get("/product-boat", function(request, response, next){
-    // response.sendFile(__dirname + '/views/product-boat.html');
-
-    response.render("product");
-
+    const boatDetails = {
+        title: "Boat",
+        price: 100,
+        imgSrc: "https://m.media-amazon.com/images/I/71iLHfq8yPL._AC_SL1500_.jpg"
+    };
+    response.render("product", boatDetails);
 });
 
 app.get("/product-jet-ski", function(request, response, next){
-    response.sendFile(__dirname + '/views/product-jet-ski.html');
+    const boatDetails = {
+        title: "Jet Ski",
+        price: 3000,
+        imgSrc: "https://checkyeti.imgix.net/images/prod/products/16829/jet-ski-hire-on-kamari-beach-in-santorini-kamari-beach-watersports-santorini.jpg"
+    };
+    response.render("product", boatDetails);
 });
 
 app.get("/product-yatch", function(request, response, next){
-    response.sendFile(__dirname + '/views/product-yatch.html');
+    const boatDetails = {
+        title: "Yatch",
+        price: 856000,
+        imgSrc: "https://yachtharbour.com/static/images/y/large_146_0d06c.jpg"
+    };
+    response.render("product", boatDetails);
 });
+
 
 app.listen(3000, () => { console.log("server listening....");});
