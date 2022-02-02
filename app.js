d3b8c642a9c3fd71ecbe395cd11cea44b94ce692
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-02-02 10:39:23 +0100

    create 3 static pages for products

--- app.js
+++ app.js
@@ -11,5 +11,19 @@ app.get("/contact", function(request, response, next){
     response.sendFile(__dirname + '/views/contact.html');
 });
 
+app.get("/product-boat", function(request, response, next){
+    // response.sendFile(__dirname + '/views/product-boat.html');
+
+    response.render("product");
+
+});
+
+app.get("/product-jet-ski", function(request, response, next){
+    response.sendFile(__dirname + '/views/product-jet-ski.html');
+});
+
+app.get("/product-yatch", function(request, response, next){
+    response.sendFile(__dirname + '/views/product-yatch.html');
+});
 
 app.listen(3000, () => { console.log("server listening....");});
