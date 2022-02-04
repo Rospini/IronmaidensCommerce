b30b092c366744405625d16a18b16f2f9aee5623
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-02-04 10:26:01 +0100

    use req.params to avoid repeating routes

--- app.js
+++ app.js
@@ -15,56 +15,63 @@ app.use(express.static('public'));
 
 // connect to DB
 mongoose
-  .connect('mongodb://localhost/ecommerceApp')
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+    .connect('mongodb://localhost/ecommerceApp')
+    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err));
 
 
-app.get("/", function(request, response, next){
+app.get("/", function (request, response, next) {
     Product.find()
-        .then( (products) => {
-            response.render("home", {products: products});
+        .then((products) => {
+            response.render("home", { products: products });
         })
-        .catch( error => console.log("error getting data from DB", error));
+        .catch(error => console.log("error getting data from DB", error));
 });
 
-app.get("/contact", function(request, response, next){
+app.get("/contact", function (request, response, next) {
     response.render("contact");
 });
 
 
-app.get("/product-boat", function(request, response, next){
-    const boatDetails = {
-        title: "Boat",
-        price: 100,
-        seller: {
-            name: "bob",
-            rating: 4.5
-        }
-    };
-    response.render("product", boatDetails);
-});
 
-app.get("/product-jet-ski", function(request, response, next){
-    const jetSkiDetails = {
-        title: "Jet Ski",
-        price: 3000,
-        imgSrc: "https://checkyeti.imgix.net/images/prod/products/16829/jet-ski-hire-on-kamari-beach-in-santorini-kamari-beach-watersports-santorini.jpg",
-        categories: ["leisure", "sports"]
-    };
-    response.render("product", jetSkiDetails);
-});
 
-app.get("/product-yatch", function(request, response, next){
-    const yatchDetails = {
-        title: "Yatch",
-        price: 856000,
-        imgSrc: "https://yachtharbour.com/static/images/y/large_146_0d06c.jpg",
-        categories: ["luxury", "bart", "toys"]
-    };
-    response.render("product", yatchDetails);
+app.get("/:productTitle", function (request, response, next) {
+    Product.findOne({title: request.params.productTitle})
+        .then(productFromDB => {
+            response.render("product", productFromDB);
+        })
+        .catch(error => console.log("error getting data from DB", error));
 });
 
 
 
-app.listen(3000, () => { console.log("server listening....");});
+// app.get("/product-boat", function (request, response, next) {
+//     Product.findOne({ title: "basic boat" })
+//         .then(productFromDB => {
+//             response.render("product", productFromDB);
+//         })
+//         .catch(error => console.log("error getting data from DB", error));
+// });
+
+
+// app.get("/product-jet-ski", function (request, response, next) {
+//     Product.findOne({ title: "jet ski" })
+//         .then(productFromDB => {
+//             console.log(productFromDB);
+//             response.render("product", productFromDB);
+//         })
+//         .catch(error => console.log("error getting data from DB", error));
+// });
+
+
+// app.get("/product-yatch", function (request, response, next) {
+//     Product.findOne({ title: "yatch" })
+//         .then(productFromDB => {
+//             response.render("product", productFromDB);
+//         })
+//         .catch(error => console.log("error getting data from DB", error));
+// });
+
+
+
+app.listen(3000, () => { console.log("server listening...."); });
